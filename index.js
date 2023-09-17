c722ee34e5b2935d73e95bb0a982e4696620fd80
poojakasar <96623437+poojabalukasar@users.noreply.github.com>
2023-09-17 15:53:23 +0530

    code for query selector

--- index.js
+++ index.js
@@ -6,7 +6,18 @@
 // elem.style.fontWeight = "bold";
 // elem.style.color = "red";
 
-let elem = document.getElementsByTagName("li");
-Array.from(elem).forEach((item) => {
-  item.style.color = "red";
+// let elem = document.getElementsByTagName("li");
+// Array.from(elem).forEach((item) => {
+//   item.style.color = "red";
+// });
+
+// let elements = document.querySelectorAll("li");
+// elements[1].style.color = "green";
+
+let elements = document.querySelectorAll("li:nth-child(odd)");
+
+Array.from(elements).forEach((item) => {
+  if (item % 2 != 0) {
+    item.style.backgroundColor = "green";
+  }
 });
