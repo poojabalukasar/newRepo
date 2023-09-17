80ab75ae6d35692a5b67a96a98b490b342be0d70
poojakasar <96623437+poojabalukasar@users.noreply.github.com>
2023-09-17 14:56:22 +0530

    new code added

---
+++ index.js
@@ -0,0 +1,7 @@
+let child = document.getElementsByClassName("list-group-item");
+
+child[2].style.backgroundColor = "green";
+
+let elem = document.querySelectorAll(".list-group-item");
+elem.style.fontWeight = "bold";
+elem.style.color = "red";
