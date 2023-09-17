ed3b47b09e71997511524a5d5d62c6ee939cb536
poojakasar <96623437+poojabalukasar@users.noreply.github.com>
2023-09-17 15:28:26 +0530

    tag name code added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let child = document.getElementsByClassName("list-group-item");
+// let child = document.getElementsByClassName("list-group-item");
 
-child[2].style.backgroundColor = "green";
+// child[2].style.backgroundColor = "green";
 
-let elem = document.querySelectorAll(".list-group-item");
-elem.style.fontWeight = "bold";
-elem.style.color = "red";
+// let elem = document.querySelectorAll(".list-group-item");
+// elem.style.fontWeight = "bold";
+// elem.style.color = "red";
+
+let elem = document.getElementsByTagName("li");
+Array.from(elem).forEach((item) => {
+  item.style.color = "red";
+});
